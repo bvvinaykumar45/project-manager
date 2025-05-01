28a91c0944c623e9d6db47f5c0b43ef3cee67677
bvvinaykumar45 <bvvinaykumar45@gmail.com>
2025-05-01 18:35:19 +0530

    minor change in log message.

--- src/db/index.js
+++ src/db/index.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    console.log(`Mongo DB connection successful`);
+    console.log(`✅ Mongo DB connection successful`);
   } catch (error) {
     console.error(error);
     process.exit(1);
